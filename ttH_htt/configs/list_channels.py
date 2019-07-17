c97e471e9d1e898dc9a47de1e926c3476860e030
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2019-07-17 12:46:56 +0200

    refactoring

---
+++ ttH_htt/configs/list_channels.py
@@ -0,0 +1,32 @@
+def list_channels(analysis) :
+    if analysis == "ttH" :
+        sigs = ["ttH", "tHq", "tHW"] #, "VH"]
+        decays = ["hww", "hzz", "htt", "hzg", "hmm" ]
+        #higgs_procs = [ [y + "_" + x  for x in decays_ttH if not (x in ["hzg", "hmm"] and y != "ttH")] for y in sigs_ttH]
+        higgs_procs = [ [y + "_" + x  for x in decays if not (x in ["hzz", "htt", "hzg", "hmm"] and y != "ttH")] for y in sigs]
+        ## add the H processes (that shall be marked as signal on the datacards)
+
+        info_channel = {
+        "2lss_0tau" : { "bkg_proc_from_data" : ["fakes_data", "flips_data"], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False}, 
+        "ttWctrl"   : { "bkg_proc_from_data" : ["fakes_data", "flips_data"], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False},
+        "2lss_1tau" : { "bkg_proc_from_data" : ["fakes_data", "flips_data"], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : True},
+        "3l_0tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False},
+        "1l_2tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False},
+        "ttZctrl"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False},
+        "2l_2tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False},
+        "3l_1tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : True}, 
+        "1l_2tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False},
+        "2los_1tau" : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares", "conversions"], "isSMCSplit" : False},
+        "0l_2tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares"],                "isSMCSplit" : False},
+        "1l_1tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares"],                "isSMCSplit" : False},
+        "WZctrl"    : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares"],                "isSMCSplit" : False},
+        "4l_0tau"   : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares"],                "isSMCSplit" : False},
+        "ZZctrl"    : { "bkg_proc_from_data" : ["fakes_data"              ], "bkg_procs_from_MC"  : ["TTW", "TTWW", "TTZ", "EWK", "Rares"],                "isSMCSplit" : False},
+        }
+    else : sys.exit("analysis " + analysis + " not implemented")
+    return {
+        "higgs_procs"      : higgs_procs,
+        "decays"           : decays,
+        "info_bkg_channel" : info_channel 
+    }
+
