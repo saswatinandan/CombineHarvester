2fd108a7852e954046d5283c52fe960067e150e3
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2020-06-26 15:06:03 +0300

    add bbWW options

--- ttH_htt/configs/list_channels.py
+++ ttH_htt/configs/list_channels.py
@@ -67,14 +67,22 @@ def list_channels(analysis, fake_mc) :
             "isSMCSplit" : False,
             "proc_to_remove" :
                 {
-                "2017" : ["ZH_htt"]
+                "2016" : ["tHq_htt", "ttH_hww", "ttH_hzz", "TTWW", "WH_htt", "ZH_htt", "ZH_hww"],
+                "2017" : ["ZH_htt", "HH_ttww", "tHq_htt", "tHW_htt", "ttH_hww", "ttH_hzz", "TTWW", "WH_htt", ],
+                "2018" : ["ggH_hzz", "tHq_htt", "tHW_hww", "ttH_hww", "ttH_hzz", "ZH_htt", "ZH_hww"]
+
                 }
             },
         "2l_2tau"   : {
             "bkg_proc_from_data" : [ fakes       ],
             "bkg_procs_from_MC"  : [ "TTW", "TTWW", "TTZ", "Rares", "WZ", "ZZ"],
             "isSMCSplit" : False,
-            "proc_to_remove" : {}
+            "proc_to_remove" : {
+            "2016" : ["ttH_hww", "tHW_htt", "TTWW", "Rares", "ZZ"],
+            "2017" : ["ttH_hzz", "TTWW"],
+            "2018" : []
+
+            }
             },
         "3l_1tau"   : {
             "bkg_proc_from_data" : [ fakes, flips   ],
@@ -90,20 +98,24 @@ def list_channels(analysis, fake_mc) :
         },
         "0l_2tau"   : {
             "bkg_proc_from_data" : [ fakes      ],
-            "bkg_procs_from_MC"  : [ "TTW", "TTWW", "TTZ", "Rares", "WZ", "ZZ", "DY", "TT"],
+            "bkg_procs_from_MC"  : [ "TTW", "TTZ", "Rares", "WZ", "ZZ", "DY", "TT"], # "TTWW",
             "isSMCSplit" : False,
             "proc_to_remove" :
             {
-                "2018" : [ "WH_htt", "ttH_hzz", "ttH_hww"], # 
-                "2017" : ["ggH_hzz", "ttH_hzz", "ttH_hzg"],
-                "2016" : ["WH_htt", "ZH_htt", "ZH_hww"]
+                "2018" : ["ggH_hww", "ggH_hzz", "ZH_hww", "WH_htt", "ttH_hzz", "ttH_hww", "TTW", "TTWW"], #
+                "2017" : ["ggH_hzz", "ttH_hzz", "ttH_hww", "ttH_hzg", "HH_tttt", "TTW", "TTWW"],
+                "2016" : ["ggH_hww", "WH_htt", "ZH_htt", "ZH_hww", "ZH_hzz", "HH_tttt", "HH_ttww", "HH_ttzz", "ttH_hzg", "ttH_hzz", "TTW", "TTWW"]
             }
             },
         "1l_1tau"   : {
             "bkg_proc_from_data" : [ fakes      ],
             "bkg_procs_from_MC"  : [ "TTW", "TTWW", "TTZ", "Rares", "WZ", "ZZ", "DY", "TT"],
             "isSMCSplit" : False,
-            "proc_to_remove" : {}
+            "proc_to_remove" : {
+            "2018" : ["ggH_hzz", "ggH_hww", "qqH_hww", "qqH_hzz", "tHq_hzz",  "ttH_hmm", "ttH_hzg", "WH_hww", "WH_hzz", "ZH_htt", "ZH_hww", "ZH_hzz", ], #
+            "2017" : ["ggH_hzz", "ggH_hww", "qqH_hww", "HH_tttt", "HH_ttww", "HH_ttzz", "tHq_hzz", "tHW_hzz", "ttH_hzg", "WH_htt", "WH_hww", "WH_hzz", "ZH_hww", "ZH_hzz"],
+            "2016" : ["ggH_hww", "qqH_hww", "tHq_hww", "tHq_hzz",  "tHq_hzz", "ttH_hmm", "ttH_hzg", "WH_htt", "WH_hzz" , "ZH_htt", "ZH_hww", "ZH_hzz",  "HH_tttt", "HH_ttww", "HH_ttzz"]
+            }
             },
         "4l_0tau"   : {
             "bkg_proc_from_data" : [ fakes, flips   ],
@@ -111,7 +123,40 @@ def list_channels(analysis, fake_mc) :
             "isSMCSplit" : False,
             "proc_to_remove" : {}
         }
-    }
+        }
+    elif analysis == "HH" :
+        #sigs = [["signal_ggf_nonresonant_cHHH1_hh_bbtt", "signal_ggf_nonresonant_cHHH1_hh_bbvv_sl", "signal_ggf_nonresonant_cHHH1_hh_bbvv"]]
+        #sigs = [["signal_ggf_nonresonant_hh_bbttSM", "signal_ggf_nonresonant_hh_bbvv_slSM", "signal_ggf_nonresonant_hh_bbvvSM" ]]
+        #sigs = [["signal_ggf_nonresonant_hh_bbttBM12", "signal_ggf_nonresonant_hh_bbvv_slBM12", "signal_ggf_nonresonant_hh_bbvvBM12" ]]
+        sigs = [["signal_ggf_spin0_900_hh_bbtt", "signal_ggf_spin0_900_hh_bbvv", "signal_ggf_spin0_900_hh_bbvv_sl"]]
+        #sigs = [["signal_ggf_nonresonant_hh_bbttkl_1p00", "signal_ggf_nonresonant_hh_bbvv_slkl_1p00", "signal_ggf_nonresonant_hh_bbvvkl_1p00"]]
+        higgs_procs = sigs
+        conversions = "Convs"
+        #print (higgs_procs)
+        if fake_mc :
+            fakes       = "fakes_mc"
+            flips       = "flips_mc"
+        else :
+            fakes       = "data_fakes"
+            flips       = "data_flips"
+
+        info_channel = {
+        "2l_0tau" : {
+            "bkg_proc_from_data" : [ fakes    ],
+            "bkg_procs_from_MC"  : ["Convs", "TTH", "TH", "TTZ", "TTW", "TTWW", "TT", "Other", "VH", "DY", "W", "WW", "WZ", "ZZ"],
+            "isSMCSplit" : False,
+            "proc_to_remove" : {}
+        },
+        "1l_0tau" : {
+            "bkg_proc_from_data" : [ fakes    ],
+            "bkg_procs_from_MC"  : ["Convs", "TTH", "TH", "TTZ", "TTW", "TTWW", "TT", "Other", "VH", "DY", "W", "WW", "WZ", "ZZ"],
+            "isSMCSplit" : False,
+            "proc_to_remove" : {}
+        }
+        }
+        decays = []
+        decays_hh = []
+
     else : sys.exit("analysis " + analysis + " not implemented")
     return {
         "higgs_procs"      : higgs_procs,
