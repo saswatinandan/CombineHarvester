237f71b7294148b19c11e8190cff044b1e727acc
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2020-07-31 04:20:03 +0300

    working rebining HH

--- ttH_htt/cards/info_2l_0tau_datacards.py
+++ ttH_htt/cards/info_2l_0tau_datacards.py
@@ -1,42 +1,67 @@
 def read_from():
-    withFolder = False
-    label = "hh_bb2l_12May_SM_default_central"
-    mom="/home/acaan/hhAnalysis/2017/"+label+"/datacards/hh_bb2l/prepareDatacards/"
-    bdtTypes = [
-        # SM_plainVars_noHH_withbb_ee_MHH1.root
-        # M_plainVars_noHH_mm_MHH5_medMbb/low/high
-        #"SM_plainVars_Xness_nocat",
-        #"SM_plainVars_noHH_nocat",
-        #"SM_plainVars_noHH_withbb_nocat",
-        #"SM_plainVars_nocat"
-        #"SM_plainVars_ee",
-        #"SM_plainVars_em",
-        #"SM_plainVars_mm",
-        "SM_plainVars_noHH_withbb_nocat",
-    ]
-    # If there are subcategories construct the list of files to read based on their naming convention
-    ## prepareDatacards_2lss_1tau_sumOS_output_NN_2lss_1tau_ttH_tH_3cat_v5_ttH_1tau.root
+    withFolderL = True
+    label  = "hh_bb1l_26Jul_baseline_TTSL_noWjj_dataMC"
 
-    channelsTypes = [ "0l_2tau" ]
+    BDTfor = "SM"
+    inclusive = True
+    in_more_subcats    = True
+    in_flavour_subcats = True
+
+    if inclusive :
+        bdtTypes =  [
+        #### one does he hadd's by hand of the categories you want to merge
+        "%s_plainVars_inclusive"             % BDTfor, #
+        ]
+    else :
+        if not in_more_subcats :
+            bdtTypes =  [
+            #### one does he hadd's by hand of the categories you want to merge
+            "%s_plainVars_cat"             % BDTfor, #
+            "%s_plainVars_cat_Hbb_boosted" % BDTfor, #
+            ]
+        else :
+            if not in_flavour_subcats :
+                bdtTypes =  [
+                #### one does he hadd's by hand of the categories you want to merge
+                "%s_plainVars_cat_1b"          % BDTfor,
+                "%s_plainVars_cat_2b"          % BDTfor,
+                #"%s_plainVars_cat_Hbb_boosted" % BDTfor,
+                ]
+            else :
+                bdtTypes = [
+                "%s_plainVars_cat_ee_1b"          % BDTfor,
+                "%s_plainVars_cat_ee_2b"          % BDTfor,
+                "%s_plainVars_cat_ee_Hbb_boosted" % BDTfor,
+                "%s_plainVars_cat_em_1b"          % BDTfor,
+                "%s_plainVars_cat_em_2b"          % BDTfor,
+                "%s_plainVars_cat_em_Hbb_boosted" % BDTfor,
+                "%s_plainVars_cat_mm_1b"          % BDTfor,
+                "%s_plainVars_cat_mm_2b"          % BDTfor,
+                "%s_plainVars_cat_mm_Hbb_boosted" % BDTfor,
+                ]
+
+    channelsTypes = [ "2l_0tau" ]
     ch_nickname = "hh_bb2l_hh_bb2l_OS"
 
     originalBinning=100
-    nbinRegular = np.arange(4, 31)
-    nbinQuant = np.arange(1, 20)
+    nbinRegular = np.arange(30, 51)
+    nbinQuant = np.arange(20, 21)
 
-    maxlim = 2.0
+    maxlim = 20.5
+    minlim = 0.0
 
     output = {
-    "withFolder"      : withFolder,
+    "withFolder"      : withFolderL,
     "label"           : label,
-    "mom"             : mom,
     "bdtTypes"        : bdtTypes,
     "channelsTypes"   : channelsTypes,
     "originalBinning" : originalBinning,
     "nbinRegular"     : nbinRegular,
     "nbinQuant"       : nbinQuant,
     "maxlim"          : maxlim,
+    "minlim"          : minlim,
     "ch_nickname"     : ch_nickname,
+    "makePlotsBin"    : [45]
     }
 
     return output
